b721d74e7374a9bdc1db917c8d17fe5d6481f07e
Christopher <csbroox@gmail.com>
2023-08-29 17:36:13 -0500

    studying how this works

--- app.js
+++ app.js
@@ -1,83 +1,57 @@
-const dataButton = document.getElementById("dataButton")
-const dataDiv = document.getElementById("dataDiv")
-dataButton.addEventListener("click",async () => {
-    const info = await fetch("https://jsonplaceholder.typicode.com/posts")
-    const data = await info.json()
-    console.log(data)
-    dataDiv.innerHTML = `
-        <div> 
-            <h1>${data.id}</h1>
-            <h2>${data.title} </h2>
-            <p> ${data.body}</p>
-        </div>
-    `
+const dataList = document.getElementById('list');
+const selectionList = document.getElementById('selection');
+// console.log(window.location.hash.slice(1))
+window.addEventListener('hashchange', () =>{
+    getEventFromHash();
+    renderSelection()
 })
-
- 
-window.addEventListener("hashchange", () => {
-    selectEntries();
-
-})
-
 
 const state = {
-    entries: [],
-    singleEntry: null
-}
-
-//whenever the hashchange event fires, this function is called
-function selectEntries(){
-    
-    getEventFromHash();
-    renderEntryDetails();
+    jSON: [],
+    selection: null
 }
 
 
-function getEventFromHash(){
-   
-    const name = window.location.hash.slice(1)
-
-  
-    const singleEntry = state.entries.find((entry) => {
-        return entry.name === name;
+async function getEventFromHash(){
+    await getData()
+    const id = window.location.hash.slice(1);
+    // console.log(id)
+    // console.log(state.jSON[1].id)
+    const selection = state.jSON.find((item) => {
+        return item.id === 1 * id;
     })
-
-    
-    state.singleEntry = singleEntry;
-    console.log(state)
-}
-
-function renderEntryDetails(){
-    if(state.singleEntry){
-        getSingleEntry();
-    }
-}
-
-const entryDiv = document.getElementById("entryDiv")
-
-
-const allEntriesDiv = document.getElementById("dataDiv")
-
-function renderEntryList() {
-    const allEntries = state.entries.map((entry) => {
-        return `<div> <a href=#${entry.name}> ${entry.name} </a> </div>`
-    })
-    allEntriesDiv.innerHTML = allEntries.join('');
+    console.log(state.jSON)
+    // console.log(selection)
+    state.selection = selection
+    console.log(state.selection)
 }
 
 
-async function getEntriesList(){
-    const info = await fetch("https://jsonplaceholder.typicode.com/posts")
-    const entryData = await info.json()
-    console.log(entryData.results)
-    state.entry = entryData.results
+async function renderSelection(){
+    await getData();
+    const selectionHTML = `
+    <h2>ID: ${state.selection.id}</h2>
+    <h4>TITLE: ${state.selection.title}</h4>
+    <p>BODY: ${state.selection.body}</p>`;
+    // console.log("selection --> ", selectionHTML)
+    selectionList.innerHTML = selectionHTML;
 }
 
 
-async function render(){
-    await getEntriesList();
-    renderEntryList();
-    selectEntries();
+
+async function getData(){
+    const info = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await info.json()
+    state.jSON = data
+    // console.log(state.jSON)
 }
 
-render();
+async function renderDataList(){
+    await getData();
+    const idHTML = state.jSON.map(element => {
+        return `<div><a href='#${element.id}'>${element.id}</a></div>`
+    }).join('')
+    dataList.innerHTML =  idHTML
+}
+
+renderDataList()
